745d3f7b0557c400d1a2eda3ab73e321575a1fce
nathan <ahrl_ahrl@naver.com>
2019-11-13 12:13:47 +0900

    first React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const pokeball = [
+  {
+    id: 1,
+    name: "bugi",
+    image: "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+  },
+  {
+    id: 2,
+    name: "isang",
+    image: "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+  },
+  {
+    id: 3,
+    name: "pairl",
+    image: "http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg",
+  }
+];
+
+function Pokemon({ name, pic, rating }) {
+  return (
+    <div>
+      <h3>나와라 {name}</h3>
+      <h4>{rating} / 5</h4>
+      <img src={pic} alt={name} width="100px"></img>
+    </div>
+  );
+}
+
+Pokemon.propTypes = {
+  name: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pokeball.map(mon => (
+        <Pokemon key={mon.id} name={mon.name} pic={mon.image} rating={mon.rating} />
+      ))}
     </div>
   );
 }
